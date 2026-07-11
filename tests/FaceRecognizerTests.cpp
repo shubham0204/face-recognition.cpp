bdbeaf23515c91c4a4734110598d36f52b0a0d15
Shubhan Panchal <panchalshubhamdev@gmail.com>
2026-07-11 09:39:56 +0530

    Add new test for recognition in FaceRecognizerTests.cpp

--- tests/FaceRecognizerTests.cpp
+++ tests/FaceRecognizerTests.cpp
@@ -7,6 +7,7 @@
 #define FACENET_MODEL_FILE_NAME "model.pte"
 
 TEST(FaceRecognizerTests, CreateInsertReadDelete) {
+    deleteResource(getResourcePath(VECTORDB_FILE_NAME));
     FaceRecognizerInternal faceRecognizer(getResourcePath(VECTORDB_FILE_NAME), getResourcePath(FACENET_MODEL_FILE_NAME));
 
     const std::string personName = "Shubham";
@@ -29,4 +30,16 @@ TEST(FaceRecognizerTests, CreateInsertReadDelete) {
     EXPECT_TRUE(results[0].faceBoundingBox.left > 0);
     EXPECT_TRUE(results[0].faceBoundingBox.right > 0);
     EXPECT_EQ(results[0].personName, personName);
+}
+
+TEST(FaceRecognizerTests, Recognition) {
+    deleteResource(getResourcePath(VECTORDB_FILE_NAME));
+    FaceRecognizerInternal faceRecognizer(getResourcePath(VECTORDB_FILE_NAME), getResourcePath(FACENET_MODEL_FILE_NAME));
+
+    faceRecognizer.insert("A", {getResourcePath("Steven_Hatfill_0001.jpg")});
+    faceRecognizer.insert("B", {getResourcePath("Sue_Grafton_0001.jpg")});
+
+    auto result = faceRecognizer.recognize(getResourcePath("Steven_Hatfill_0002.jpg"));
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0].personName, "A");
 }
