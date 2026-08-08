8ac264a9c168b01f16fc409e9c097183b91737ae
Shubhan Panchal <panchalshubhamdev@gmail.com>
2026-08-08 17:48:59 +0530

    Remove unwanted ; in VectorIndex.cpp

--- src/VectorIndex.cpp
+++ src/VectorIndex.cpp
@@ -136,7 +136,6 @@ void VectorIndex::insert(const std::string& personName, const Embedding& embeddi
 NNQueryResult VectorIndex::nearestNeighbor(const Embedding& embedding) const {
     const double n = computeNorm(embedding);
     double maxCosineSimilarity = -std::numeric_limits<double>::infinity();
-    ;
     std::string maxCosineSimilarityPersonName;
     for (const auto& record : records) {
         double cosine = computeDotProduct(record.vector, embedding) / (n * record.norm);
