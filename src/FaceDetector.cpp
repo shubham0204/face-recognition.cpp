59af58c3e3332a083a8c1fe3abd65947d91364bb
Shubhan Panchal <panchalshubhamdev@gmail.com>
2026-08-22 07:53:26 +0530

    Reduce image dimensions by 30% to speed up HOG-SVM face detection

--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -3,9 +3,19 @@
 std::vector<dlib::rectangle> FaceDetector::detectFaces(const DlibRgbImage& inputImage) {
     dlib::matrix<unsigned char> grayImage;
     dlib::assign_image(grayImage, inputImage);
-    std::vector<dlib::rectangle> faces = detector(grayImage);
+
+    const long newRows = grayImage.nr() / FACE_DETECTION_IMG_RESIZE_FACTOR;
+    const long newCols = grayImage.nc() / FACE_DETECTION_IMG_RESIZE_FACTOR;
+    dlib::matrix<unsigned char> resizedGrayImage(newRows, newCols);
+    dlib::resize_image(grayImage, resizedGrayImage, dlib::interpolate_bilinear());
+
+    std::vector<dlib::rectangle> faces = detector(resizedGrayImage);
+
+    std::ranges::transform(faces, faces.begin(),
+                           [](const dlib::rectangle& rectangle) { return dlib::scale_rect(rectangle, FACE_DETECTION_IMG_RESIZE_FACTOR); });
     return faces;
 }
+
 dlib::array<DlibRgbImage> FaceDetector::cropFaces(const DlibRgbImage& inputImage, const std::vector<dlib::rectangle>& rectangles) {
     std::vector<dlib::chip_details> chipDetails;
     chipDetails.reserve(rectangles.size());
--- src/Types.hpp
+++ src/Types.hpp
@@ -7,6 +7,7 @@
 
 #define EMBEDDING_DIM 512
 #define INPUT_IMG_DIM 160
+#define FACE_DETECTION_IMG_RESIZE_FACTOR 1.3
 using Embedding = std::array<double, EMBEDDING_DIM>;
 using DlibRgbImage = dlib::matrix<dlib::rgb_pixel>;
 
